68024d29723dafebbc05d8095ae97f33aaf61730
capricia <capriciaw16@gmail.com>
2023-03-05 17:42:48 -0600

    seeds error

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,7 +8,7 @@ INSERT INTO role (title, salary, department_id) VALUES ("Store manager", 800000,
 INSERT INTO role (title, salary, department_id) VALUES ("Team Leader", 15000, 3);
 INSERT INTO role (title, salary, department_id) VALUES ("Sales person", 120000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Siearra", "Randall", 1);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Capri", "Weston", 2);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Anthony", "December", 3);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Shawn, Glover", 4);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("Siearra", "Randall", 1);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("Capri", "Weston", 2);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("Anthony", "December", 3);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("Shawn, Glover", 4);
