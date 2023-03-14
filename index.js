83d079ea19c319348c722c4eb7b9f12fb91df2d7
capricia <capriciaw16@gmail.com>
2023-03-14 11:29:58 -0500

    error change to import

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+import mysql from "mysql2";
+
+const connection = mysql.createconnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    datatbase: 'employees_db',
+})
+
+const init = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type:'list',
+            name: 'action',
+            message: 'What do you want to do?',
+            choices: ['View department', 'View role', 'View employees', 'Add a department', 'Add a role', 'Add an employee', 'Exit']
+            
+        }
+    ]) 
+
+    console.log(answers)
+    if (answers.action === 'View department') {
+        viewDepartment()
+    } else if (answers.action === 'View role') {
+        viewrole()
+    } else if (answers.action === 'View employee') {
+        viewemployee()
+    } else if (answers.action === 'Add a department') {
+        addDepartment()
+    } else {
+        process.exit()
+    }
+}
+
+init()
